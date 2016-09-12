ac205e098ae9120c67fd094cca304e426e635f64
Wei <futurewayne@gmail.com>
2016-09-12 16:01:01 +0800

    class implementation of UI

--- myPython/SComConf.py
+++ myPython/SComConf.py
@@ -5,17 +5,6 @@ from Tkinter import *
 import Tkinter as tk
 import tkMessageBox
 
-def about():
-    tkMessageBox.showinfo("About", "Serial COM Port Configuration & Test Tool\ndesigned by Wei in Sep 2016\nEmail: futurewayne@gmail.com")
-	
-def open():
-	tkMessageBox.showinfo("Open SCom Configuration...", "not implemented yet")
-	
-def do_tx():
-	print "Start Tx now..."
-	print "COM port: %s" % port.get()
-	print "Baudrate: %s" % baudrate.get()
-
 class ScrolledText(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent)
@@ -35,10 +24,10 @@ class ScrolledText(tk.Frame):
 	
 class inputCellE (tk.Frame):
     def __init__(self, parent, ltxt, default):
-		tk.Frame.__init__(self, parent, class_='inputCellE')
-		self.pack(side=TOP)
-		self.creatWidget(ltxt, default)
-		self.get = self.p.get
+	tk.Frame.__init__(self, parent, class_='inputCellE')
+	self.pack(side=TOP)
+	self.creatWidget(ltxt, default)
+	self.get = self.p.get
     def creatWidget(self, ltxt, default):
         self.l = Label(self, text=ltxt, width=10, anchor=E, padx=5)
         self.l.pack(side=LEFT)
@@ -48,10 +37,10 @@ class inputCellE (tk.Frame):
 
 class inputCellLB (tk.Frame):
     def __init__(self, parent, ltxt, alist):
-		tk.Frame.__init__(self, parent, class_='inputCellLB')
-		self.pack(side=TOP)
-		self.creatWidget(ltxt, alist)
-		self.get = self.p.get
+	tk.Frame.__init__(self, parent, class_='inputCellLB')
+	self.pack(side=TOP)
+	self.creatWidget(ltxt, alist)
+	self.get = self.p.get
     def creatWidget(self, ltxt, alist):
         self.l = Label(self, text=ltxt, width=10, anchor=E, padx=5)
         self.l.pack(side=LEFT)
@@ -61,10 +50,10 @@ class inputCellLB (tk.Frame):
 
 class inputCellOM (tk.Frame):
     def __init__(self, parent, ltxt, alist):
-		tk.Frame.__init__(self, parent, class_='inputCellOM')
-		self.pack(side=TOP)
-		self.creatWidget(ltxt, alist)
-		self.get = self.v.get
+	tk.Frame.__init__(self, parent, class_='inputCellOM')
+	self.pack(side=TOP)
+	self.creatWidget(ltxt, alist)
+	self.get = self.v.get
     def creatWidget(self, ltxt, alist):
         self.l = Label(self, text=ltxt, width=10, anchor=E, padx=5)
         self.l.pack(side=LEFT)
@@ -72,55 +61,67 @@ class inputCellOM (tk.Frame):
         self.v.set(alist[0])
         self.p = OptionMenu(self, self.v, *alist)
         self.p.pack(side=RIGHT)
-		
+
+class mainWin (tk.Frame):
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent, class_='mainWin')
+        self.pack(fill=BOTH, expand=TRUE)
+        self.creatMenu(parent)
+        self.creatWidget()
+    def creatWidget(self):
+        pw1 = PanedWindow(self) 
+        pw1.pack(fill=BOTH, expand=1)
+        frameInput = LabelFrame(pw1, text="SCom Parameters", height=200, width=30)
+        frameInput.pack(fill="both", expand="yes")
+        pw1.add(frameInput)
+        self.port = inputCellE(frameInput, "Port:", "COM1")
+        self.baudrate = inputCellE(frameInput, "Baudrate:", "19200")
+        #FIVEBITS(5), SIXBITS(6), SEVENBITS(7), EIGHTBITS(8)
+        self.bytesize = inputCellE(frameInput, "DataBits:", "8") 
+        #PARITY_NONE(N), PARITY_EVEN(E), PARITY_ODD(O), PARITY_MARK(M), PARITY_SPACE(S)
+        self.parity = inputCellE(frameInput, "Parity:", "N")
+        #STOPBITS_ONE(1), STOPBITS_ONE_POINT_FIVE(1.5), STOPBITS_TWO(2)
+        self.stopbits = inputCellE(frameInput, "StopBits:", "1")
+        self.timeout = inputCellE(frameInput, "Timeout(s):", "10")
+        fc_items = ('NONE', 'XONXOFF', 'RTSCTS', 'DSRDTR')
+        self.flowctrl = inputCellOM(frameInput, "FlowCtrl:", fc_items)
+        
+        pw2 = PanedWindow(pw1, orient=VERTICAL)
+        pw1.add(pw2)
+        frameTransmitted = LabelFrame(pw2, text="Transmitted", width=800, height=100)
+        self.entry_tx = Entry(frameTransmitted)
+        self.entry_tx.pack(side=LEFT, fill=X, expand=1)
+        button_tx = Button(frameTransmitted, text='Send', command=do_tx)
+        button_tx.pack(side=RIGHT)
+        pw2.add(frameTransmitted)
+        frameReceived = LabelFrame(pw2, text="Received", width=800)
+        self.text_recv = ScrolledText(frameReceived)
+        self.text_recv.pack(side=TOP, fill=BOTH, expand=1)
+        pw2.add(frameReceived)
+    def creatMenu(self, parent):
+        menubar = Menu(self)
+        filemenu = Menu(menubar,tearoff=0)
+        filemenu.add_command(label="Open", command=open)
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=rootWin.quit)
+        menubar.add_cascade(label="File", menu=filemenu)
+        helpmenu = Menu(menubar, tearoff=0)
+        helpmenu.add_command(label="About", command=about)
+        menubar.add_cascade(label="Help", menu=helpmenu)
+        parent.config(menu=menubar)
+
+def about():
+    tkMessageBox.showinfo("About", "Serial COM Port Configuration & Test Tool\ndesigned by Wei in Sep 2016\nEmail: futurewayne@gmail.com")
+	
+def open():
+	tkMessageBox.showinfo("Open SCom Configuration...", "not implemented yet")
+	
+def do_tx():
+	print "Start Tx now..."
+	print "COM port: %s" % mywin.port.get()
+	print "Baudrate: %s" % mywin.baudrate.get()
 
 rootWin = tk.Tk()
 rootWin.title('Serial COM Port Configuration & Test')
-#rootWin.geometry('400x200')
-
-pw1 = PanedWindow(rootWin) 
-pw1.pack(fill=BOTH, expand=1)
-frameInput = LabelFrame(pw1, text="SCom Parameters", height=200, width=30)
-frameInput.pack(fill="both", expand="yes")
-pw1.add(frameInput)
-port = inputCellE(frameInput, "Port:", "COM1")
-baudrate = inputCellE(frameInput, "Baudrate:", "19200")
-#FIVEBITS(5), SIXBITS(6), SEVENBITS(7), EIGHTBITS(8)
-bytesize = inputCellE(frameInput, "DataBits:", "8") 
-#PARITY_NONE(N), PARITY_EVEN(E), PARITY_ODD(O), PARITY_MARK(M), PARITY_SPACE(S)
-parity = inputCellE(frameInput, "Parity:", "N")
-#STOPBITS_ONE(1), STOPBITS_ONE_POINT_FIVE(1.5), STOPBITS_TWO(2)
-stopbits = inputCellE(frameInput, "StopBits:", "1")
-timeout = inputCellE(frameInput, "Timeout(s):", "10")
-fc_items = ('NONE', 'XONXOFF', 'RTSCTS', 'DSRDTR')
-flowctrl = inputCellOM(frameInput, "FlowCtrl:", fc_items)
-
-pw2 = PanedWindow(pw1, orient=VERTICAL)
-pw1.add(pw2)
-frameTransmitted = LabelFrame(pw2, text="Transmitted", width=800, height=100)
-entry_tx = Entry(frameTransmitted)
-entry_tx.pack(side=LEFT, fill=X, expand=1)
-button_tx = Button(frameTransmitted, text='Send', command=do_tx)
-button_tx.pack(side=RIGHT)
-pw2.add(frameTransmitted)
-frameReceived = LabelFrame(pw2, text="Received", width=800)
-text_recv = ScrolledText(frameReceived)
-text_recv.pack(side=TOP, fill=BOTH, expand=1)
-pw2.add(frameReceived)
-
-
-# Menu configuration starts here
-menubar = Menu(rootWin)
-filemenu = Menu(menubar,tearoff=0)
-filemenu.add_command(label="Open", command=open)
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=rootWin.quit)
-menubar.add_cascade(label="File", menu=filemenu)
-
-helpmenu = Menu(menubar, tearoff=0)
-helpmenu.add_command(label="About", command=about)
-menubar.add_cascade(label="Help", menu=helpmenu)
-
-rootWin.config(menu=menubar)
-
+mywin = mainWin(rootWin)
 rootWin.mainloop()
